844c5b805fc3df82b6d74e835db87aa86edf5861
chi-ino <cra.00x@gmail.com>
2021-12-13 01:09:26 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   
-  
+  validates  :name, null: false
+  validates  :explanation, null: false 
+  validates  :category_id, null: false
+  validates  :status_id, null: false
+  validates  :postage_id, null: false
+  validates  :prefecture_id, null: false
+  validates  :shipping_day_id, null: false
+  validates  :price, null: false
+  validates  :user, null: false
+
 
 end
