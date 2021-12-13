099d965502674a8128e5c3cfe9e015354ae9ab00
chi-ino <cra.00x@gmail.com>
2021-12-13 14:34:29 +0900

    空文字は許可されないように訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,22 +11,23 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates  :name, null: false
-  validates  :explanation, null: false 
-  validates  :category_id, null: false
-  validates  :status_id, null: false
-  validates  :postage_id, null: false
-  validates  :prefecture_id, null: false
-  validates  :shipping_day_id, null: false
-  validates  :price, null: false
-  validates  :user, null: false
-  
+  validates  :name, presence: true
+  validates  :explanation, presence: true
+  validates  :category_id, presence: true
+  validates  :status_id, presence: true
+  validates  :postage_id, presence: true
+  validates  :prefecture_id, presence: true
+  validates  :shipping_day_id, presence: true
+  validates  :price, presence: true
+  validates  :user, presence: true
+
   # ジャンルの選択が「--」の時は保存不可
   validates :category_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :postage_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_day_id, numericality: { other_than: 1 }
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
 
 
 end
