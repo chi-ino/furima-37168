c57e3bf56434763ff3fdc71177bdecac83f12858
chi-ino <cra.00x@gmail.com>
2021-12-13 11:22:20 +0900

    ストロングパラメーターのセット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :price, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 end
