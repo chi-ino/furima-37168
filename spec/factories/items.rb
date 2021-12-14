ca13fd3cb21e28e1aaa9f1dcb3092fb87a6345bb
chi-ino <cra.00x@gmail.com>
2021-12-14 10:36:27 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :item do
     name            { '商品名' }
+    explanation     { '説明文' }
+    price           { 2000 }
     category_id     { 2 }
     status_id       { 2 }
     postage_id      { 2 }
