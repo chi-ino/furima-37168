2b3b6b64de01d0c0d0f44d5a2277f83346478313
chi-ino <cra.00x@gmail.com>
2021-12-15 15:39:36 +0900

    showアクションのコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, only: [:new]
+  #
+  before_action :move_to_index, except: [:index, :show]
+
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,10 +22,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :price, :category_id, :status_id, :postage_id, :prefecture_id,
                                  :shipping_day_id).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
