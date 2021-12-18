205102f7fef7b63250c62252d7bb385d82aa6be8
chi-ino <cra.00x@gmail.com>
2021-12-18 21:49:01 +0900

    バリデーションの訂正

--- app/models/card_buyer.rb
+++ app/models/card_buyer.rb
@@ -1,14 +1,16 @@
 class CardBuyer
-  ActiveModel::Model
+  include ActiveModel::Model
   attr_accessor :user_id, :item_id, :text, :price, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
-    validates :postal_code 
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}  
+    validates :city
     validates :address
-    validates :building
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :item_id
+    validates :user_id
   end
-  validates :prefecture_id
+    validates  :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   
   def save
     card = Card.create(user_id: user_id, item_id: item_id)
