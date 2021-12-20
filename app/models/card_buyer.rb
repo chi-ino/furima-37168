23c362c105253ec6f1dd4bb6c5c2cc722f399dd9
chi-ino <cra.00x@gmail.com>
2021-12-20 10:45:05 +0900

    出品機能の訂正

--- app/models/card_buyer.rb
+++ app/models/card_buyer.rb
@@ -4,14 +4,15 @@ class CardBuyer
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :item_id
     validates :user_id
-    validates :token, presence: true
+    validates :token
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  
 
   def save
     card = Card.create(user_id: user_id, item_id: item_id)
